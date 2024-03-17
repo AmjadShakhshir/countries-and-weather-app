e6185b11a81324fb2535ea6ce56d3279ea72e041
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-17 16:33:02 +0200

    add styling and add bootstrap to the component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 import country from "../services/country";
+import { Card } from "react-bootstrap";
 
 const Weather = ({ capital, api_key }) => {
   const [weather, setWeather] = useState({});
@@ -10,12 +11,20 @@ const Weather = ({ capital, api_key }) => {
     });
   }, [api_key, capital]);
   return (
-    <div>
-      <p>Weather</p>
-      <p>temperature: {weather.main?.temp} Celsius</p>
-      <img height={100} width={100} src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`} alt="weather icon" />
-      <p>wind: {weather.wind?.speed} m/s</p>
-    </div>
+    <Card className="card">
+      <Card.Header>Weather in {capital}</Card.Header>
+      <Card.Body>
+        <Card.Text>
+          <strong>Temperature:</strong> {weather.main?.temp} Celsius
+        </Card.Text>
+        <Card.Text>
+          <img height={100} width={100} src={`http://openweathermap.org/img/wn/${weather.weather?.[0].icon}.png`} alt="weather icon" />
+        </Card.Text>
+        <Card.Text>
+          <strong>Wind:</strong> {weather.wind?.speed} m/s
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
